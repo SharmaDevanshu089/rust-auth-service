8adc9117f931133f4bed38baff0f46739304bdf2
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-01 18:33:53 +0530

    Added A funtion which basic returns and now calling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::Router;
+use axum::{Router, routing::get};
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -6,5 +6,13 @@ async fn main() {
     //YE LOCALHOST PE 3000 PORT PE SOCKET BANAYEGA
     let websocket_address = SocketAddr::from(([0, 0, 0, 0], 3000));
     // ABHI EK TEMPORARY ROUTER BANA RAHA HU TEST KE LIYE BAAD ME ACCHE SE LIKH DUNGA
-    let axium_router = Router::new();
+    // YE ROUTER ABHI KEVAL HELLO VALE KO CALL KAR RAH H
+    let axium_router = Router::new().route("/", get(return_hello()));
+}
+
+// YE KEVAL TESTING KE LIYE H , KUCH KAAM KA NAHI HAI SIRF HELLO RETURN KARTA HAI
+// MUJHE TEST KARNA THA ISS LIYE PUT KIA
+async fn return_hello() -> String {
+    let hello = "Hello Via Internet".to_string();
+    return hello;
 }
