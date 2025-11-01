4536ea0c55bf07d3e171484b9505c90597e20890
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-01 18:47:28 +0530

    Added Server but now working

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,16 @@ use std::net::SocketAddr;
 async fn main() {
     //YE LOCALHOST PE 3000 PORT PE SOCKET BANAYEGA
     let websocket_address = SocketAddr::from(([0, 0, 0, 0], 3000));
+
     // ABHI EK TEMPORARY ROUTER BANA RAHA HU TEST KE LIYE BAAD ME ACCHE SE LIKH DUNGA
     // YE ROUTER ABHI KEVAL HELLO VALE KO CALL KAR RAH H
     let axium_router = Router::new().route("/", get(return_hello()));
+
+    // AB ISS PORT KO BIND KARUNGA, ABHI ERROR KO UNWRAP KAR RAHA HU BAAD ME LOG KAURNGA
+    axum::Server::bind(&websocket_address)
+        .serve(axium_router.into_make_service())
+        .await
+        .unwrap();
 }
 
 // YE KEVAL TESTING KE LIYE H , KUCH KAAM KA NAHI HAI SIRF HELLO RETURN KARTA HAI
