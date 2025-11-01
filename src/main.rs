abae52e18b7a4e4c222e88ecddfc46721eb5b312
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-01 18:28:03 +0530

    Created Router

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use axum::Router;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     //YE LOCALHOST PE 3000 PORT PE SOCKET BANAYEGA
     let websocket_address = SocketAddr::from(([0, 0, 0, 0], 3000));
+    // ABHI EK TEMPORARY ROUTER BANA RAHA HU TEST KE LIYE BAAD ME ACCHE SE LIKH DUNGA
+    let axium_router = Router::new();
 }
