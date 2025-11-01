639fcf3c73167adb738453458d893b8fcbcfab51
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-01 18:51:25 +0530

    Now using tcp lisner

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::Router;
 use axum::routing::get;
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
@@ -12,6 +13,8 @@ async fn main() {
     let axium_router = Router::new().route("/", get(return_hello()));
 
     // AB ISS PORT KO BIND KARUNGA, ABHI ERROR KO UNWRAP KAR RAHA HU BAAD ME LOG KAURNGA
+
+    // ABHI DOCS READ KIE NEW AXUM ME SERVER KI JAGAH SERVE USE HOTA HAI AUR AB TOKIO KA TCP LISNER USE HOGA
     axum::Server::bind(&websocket_address)
         .serve(axium_router.into_make_service())
         .await
@@ -20,7 +23,7 @@ async fn main() {
 
 // YE KEVAL TESTING KE LIYE H , KUCH KAAM KA NAHI HAI SIRF HELLO RETURN KARTA HAI
 // MUJHE TEST KARNA THA ISS LIYE PUT KIA
-async fn return_hello() -> String {
-    let hello = "Hello Via Internet".to_string();
+async fn return_hello() -> &'static str {
+    let hello = "Hello Via Internet";
     return hello;
 }
