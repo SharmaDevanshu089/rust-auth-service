4fba1ef26fad6ad7cad1c370d9ab3d3ca7d0dc2a
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-02 17:06:51 +0530

    Added User Service

--- src/services.rs
+++ src/services.rs
@@ -13,4 +13,28 @@ pub mod user_service {
     use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
     use std::env;
     use tracing::error;
+    pub async fn create_user(email_in: String, hash_in: String) -> User {
+        // PURNANA DATABASE URL NIKAL LIYA
+        let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let mut conn = AsyncPgConnection::establish(&db_url)
+            .await
+            .expect("Failed to connect to database");
+
+        //NAYE USER KE LIYE PAYLOAD
+        let new_user = NewUser {
+            email: email_in,
+            password_hash: hash_in,
+        };
+
+        // USING DEISEL FOR SQL QUERY OF INSERT INTO TABLE TABLENAME
+        let user = diesel::insert_into(users)
+            .values(&new_user)
+            // ISSE RESULT VAPIS RETUNR HOGA CONNECTION KA
+            .get_result::<User>(&mut conn)
+            .await
+            .expect("Error saving new user");
+
+        //USER KO RETURN KARNA HAI
+        user
+    }
 }
