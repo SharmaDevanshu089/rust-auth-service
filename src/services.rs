f8046cccd38c1c18fc2980695fb6f304e0ad5e09
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-02 17:00:38 +0530

    added import in module

--- src/services.rs
+++ src/services.rs
@@ -5,4 +5,12 @@ use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
 use std::env;
 use tracing::error;
 
-pub mod user_service {}
+pub mod user_service {
+    use crate::models::{NewUser, User};
+    // saare import kar raha hu kyoki tabhi kaam karega (eg me same hai)
+    use crate::schema::users::dsl::*;
+    use diesel::prelude::*;
+    use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
+    use std::env;
+    use tracing::error;
+}
