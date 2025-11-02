ea07f961648eda5628e54dd9f5642a7b075c053d
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-02 15:35:09 +0530

    Some Changes

--- src/models.rs
+++ src/models.rs
@@ -4,3 +4,13 @@ use chrono::DateTime;
 use chrono::Utc;
 use diesel::prelude::*;
 use uuid::Uuid;
+
+// ABI YE STRUCT BANA RAHA HU MERE DATABSE ME SE UTAR KE
+#[derive(Queryable, Selectable, Debug)]
+#[diesel(table_name = users)]
+pub struct Users {
+    pub id: Uuid,
+    pub email: String,
+    pub password_hash: String,
+    pub created_at: DateTime<Utc>,
+}
