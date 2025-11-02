02d8265b9195c240b3e82d8f480a9e488452cebb
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-02 19:46:04 +0530

    ADDED CLAIMS IN AUTH

--- src/auth.rs
+++ src/auth.rs
@@ -1,1 +1,9 @@
 // yaha auth inset logic hoga
+use serde::{Deserialize, Serialize};
+
+// HANDLER SE YAHA LAA RAHA HU EASY NESS FOR PROGRAM
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Claims {
+    pub sub: String,
+    pub exp: usize,
+}
