9e3c295bcf859781885899e6a0c51d9383fffed1
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-11-02 16:58:32 +0530

    Writing in a module

--- src/services.rs
+++ src/services.rs
@@ -2,6 +2,7 @@ use crate::models::{NewUser, User};
 use crate::schema::users;
 use diesel::prelude::*;
 use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
-
 use std::env;
 use tracing::error;
+
+pub mod user_service {}
